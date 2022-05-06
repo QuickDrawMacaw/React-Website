38da2dd0a6e4df30b78165d4d7d5e4cf342d361e
Yuki Tabata <ytabata11@gmail.com>
2022-05-06 13:53:16 +1000

    commit

---
+++ src/components/tableBody.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TableBody extends Component {
+    render() { 
+        return (
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        );
+    }
+}
+ 
+export default TableBody;
