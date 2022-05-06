f705473717fceb5e0f98cab211be23af7b0cf92a
Yuki Tabata <ytabata11@gmail.com>
2022-05-06 12:50:58 +1000

    commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import ListGroup from './listGroup';
-import Like from './common/like';
+import MoviesTables from './moviesTables';
+
 import Pagination from './pagination';
 import { paginate } from '../utils/paginate';
 import { getGenres } from '../services/fakeGenreService';
@@ -70,32 +71,10 @@ class Movies extends Component {
                 </div>
                 <div className="col">
                     <p>Showing { filteredMovies.length } movies in the database.</p>
-
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th/>
-                                <th/>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(movie => 
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
-                                </td>
-                                <td><button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>Delete</button></td>
-                            </tr>)}
-                        </tbody>
-                    </table>
+                    <MoviesTables
+                     movies={movies}
+                     onDelete={this.handleDelete}
+                     onLike={this.handleLike} />
                     <Pagination 
                     itemsCount={filteredMovies.length} 
                     pageSize={pageSize}
